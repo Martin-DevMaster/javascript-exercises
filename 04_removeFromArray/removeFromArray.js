71349594a59f5d668894bc1aab82ad310a66f475
Martin-DevMaster <martin.gjavol96@gmail.com>
2023-05-20 15:11:06 +0200

    removeFromArray completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    for(let i = 1; i < arguments.length; i ++) {
+        for(let j = 0; j < arr.length; j++) {
+            if(arguments[i] === arr[j]) {
+                arr.splice(j, 1)
+            }
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
