4c406661ba565be084abece7a407ac6563fa964e
Martin-DevMaster <martin.gjavol96@gmail.com>
2023-05-22 16:10:02 +0200

    sumAll completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,10 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b)) {return 'ERROR'}
+    if(a < 0 || b < 0 ) {return 'ERROR'}
+    if(Array.isArray(a) || Array.isArray(b)){return 'ERROR'}
+    return (Math.abs(a-b) + 1)*(a + b)/2;
+ };
+ 
 
 // Do not edit below this line
 module.exports = sumAll;
