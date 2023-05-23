93727091ff8481916800855ff0d310bb8d8ce0b7
Martin-DevMaster <martin.gjavol96@gmail.com>
2023-05-23 14:35:39 +0200

    leapYears completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if(years % 100 === 0 && years % 400 !== 0) {return false}
+    if(years % 4 === 0) {return true}
+    else {return false};
 };
 
 // Do not edit below this line
