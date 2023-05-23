70c51f93298bebb5766436dcc28de9df95f24b15
Martin-DevMaster <martin.gjavol96@gmail.com>
2023-05-23 16:18:48 +0200

    tempConversion completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsConv = (fahrenheit - 32)* 5 / 9 
+  if(isNaN(fahrenheit)) {return false} 
+  else return Math.round(celsConv * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrRound = celsius * 1.8 + 32
+  if(isNaN(celsius)) {return false}
+  else return Math.round(fahrRound * 10) / 10
 };
 
 // Do not edit below this line
